7381ebe018532cab42deca52515044d2ff770422
IwanVonNeumann <grabageheap@inbox.lv>
2018-02-04 12:04:11 +0200

    img in and out directories split

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from colors import get_unique_colors, replace_colors
 
 K = 64
 
-images_dir = "img"
-source_img_file = "apples_400.jpg"
-# source_img_file = "boat_400.png"
+images_dir_in = "img_in"
+images_dir_out = "img_out"
 
-source_img_path = os.path.join(images_dir, source_img_file)
+source_img_file = "apples_400.jpg"
+
+source_img_path = os.path.join(images_dir_in, source_img_file)
 img = imageio.imread(source_img_path)
 
 unique_colors = list(get_unique_colors(img))
@@ -26,7 +27,6 @@ color_mean_map = make_color_mean_map(unique_colors, final_clusters)
 new_img = replace_colors(img, color_mean_map)
 
 result_img_file = "apples_out_400.png"
-# result_img_file = "boat_out_400.png"
 
-result_img_path = os.path.join(images_dir, result_img_file)
+result_img_path = os.path.join(images_dir_out, result_img_file)
 imageio.imwrite(result_img_path, new_img)
